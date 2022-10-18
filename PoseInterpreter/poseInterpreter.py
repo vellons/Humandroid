69fa05242679236d7b6186bf05c95f06ecfc31d1
vellons <vellons@users.noreply.github.com>
2022-10-19 01:41:13 +0200

    Now support mediapipe==0.8.11 with python 3.9 Also improvements on drawing

--- PoseInterpreter/poseInterpreter.py
+++ PoseInterpreter/poseInterpreter.py
@@ -2,9 +2,11 @@ import cv2
 import math
 import mediapipe as mp
 from mediapipe.python.solutions.pose import PoseLandmark
-from mediapipe.python.solutions.drawing_utils import DrawingSpec
-from mediapipe.python.solutions.drawing_utils import VISIBILITY_THRESHOLD
+from mediapipe.python.solutions.drawing_utils import DrawingSpec, _normalize_color
+from mediapipe.python.solutions.drawing_utils import _VISIBILITY_THRESHOLD, _PRESENCE_THRESHOLD
+from mediapipe.python.solutions.drawing_styles import _POSE_LANDMARKS_LEFT, _POSE_LANDMARKS_RIGHT
 import numpy as np
+import matplotlib.pyplot as plt
 import json
 
 from PoseInterpreter.body_landmark import PoseInterpreterBodyLandmark
@@ -13,42 +15,64 @@ from PoseInterpreter.body_landmark import PoseInterpreterBodyLandmark
 class PoseInterpreter:
     """
     Human pose detection to control humandroid robots.
-    Works with MediaPipe technology
+    Works with MediaPipe technology.
+    This class works with RGB values.
 
     Please refer to https://mediapipe.dev/ for more info about the pose processing.
     """
 
     _calc_z = False
-    _upper_body_only = False
 
     _mp_solution_pose = mp.solutions.pose
     _mp_solution_drawing = mp.solutions.drawing_utils
-    _mp_pose = None  # MediaPipe object
+    _mp_drawing_styles = mp.solutions.drawing_styles
+    _mp_pose = None  # MediaPipe result object
 
     detected_pose = None  # Data directly form MediaPipe
     computed_pose = None  # Computed data for humandroid robot with servo angles
     computed_ptp = {}  # Computed point to point pose (for humandroid robot motors)
 
     _plt_fig = None  # Used inside draw_3d_environment()
+    _plt_azim = 1
 
-    def __init__(self, config_path: str, static_image_mode: bool = False, upper_body_only: bool = False,
-                 face_connections: bool = True, calc_z: bool = False):
+    _FACE_LANDMARKS = frozenset([
+        PoseLandmark.RIGHT_EYE_INNER, PoseLandmark.RIGHT_EYE,
+        PoseLandmark.RIGHT_EYE_OUTER, PoseLandmark.RIGHT_EAR, PoseLandmark.MOUTH_RIGHT,
+        PoseLandmark.LEFT_EYE_INNER, PoseLandmark.LEFT_EYE,
+        PoseLandmark.LEFT_EYE_OUTER, PoseLandmark.LEFT_EAR, PoseLandmark.MOUTH_LEFT,
+    ])
+
+    # Style configuration
+    POSE_LANDMARKS_THICKNESS = 8
+    POSE_LANDMARKS_CIRCLE_RADIUS = 2
+    POSE_LANDMARKS_LEFT_COLOR = (0, 138, 255)
+    POSE_LANDMARKS_RIGHT_COLOR = (255, 165, 0)
+    POSE_LANDMARKS_NOSE_COLOR = (255, 48, 48)
+
+    ANGLE_TEXT_FONT = 1.2
+    ANGLE_TEXT_THICKNESS = 4
+    ANGLE_TEXT_MARGIN = 50
+    ANGLE_TEXT_COLOR = (255, 0, 0)
+    ANGLE_Z_TEXT_COLOR = (255, 255, 255)
+    ANGLE_MATH_TEXT_COLOR = (0, 0, 255)
+
+    PLOT_COLOR = (24, 240, 24)
+    PLOT_THICKNESS = 2
+
+    def __init__(self, config_path: str, static_image_mode: bool = False,
+                 display_face_connections: bool = True, calc_z: bool = False):
         """
-        Initializes Humandroid object and MediaPipe.
+        Initializes PoseInterpreter object and MediaPipe.
 
         Args:
           static_image_mode: Whether to treat the input images as a batch of static
             and possibly unrelated images, or a video stream. See details in
             https://solutions.mediapipe.dev/pose#static_image_mode.
-          upper_body_only: Whether to track the full set of 33 pose landmarks or
-            only the 25 upper-body pose landmarks. See details in
-            https://solutions.mediapipe.dev/pose#upper_body_only.
         """
 
         print("PoseInterpreter initialization")
         self.config_path = config_path
-        self._upper_body_only = upper_body_only
-        self._face_connections = face_connections
+        self._display_face_connections = display_face_connections
         self._calc_z = calc_z
 
         try:
@@ -58,34 +82,31 @@ class PoseInterpreter:
                     angle = (PoseLandmark[j["points"][0]], PoseLandmark[j["points"][1]], PoseLandmark[j["points"][2]])
                     j["pose_landmarks"] = angle
         except Exception as e:
-            print("Failed to load configuration. Impossible to calc angles. Error: {}".format(e))
-            self.configuration = {
-                "joints": {}
-            }
+            raise Exception("Failed to load configuration. Impossible to calc angles. Error: {}".format(e))
 
         # print("Configuration", self.configuration)
 
         self._mp_connections = self._mp_solution_pose.POSE_CONNECTIONS
-        if self._upper_body_only:
-            self._mp_connections = self._mp_solution_pose.UPPER_BODY_POSE_CONNECTIONS
 
-        if not self._face_connections:
+        if not self._display_face_connections:
             self._mp_connections = list(self._mp_connections)
-            self._mp_connections.remove((PoseLandmark.NOSE, PoseLandmark.RIGHT_EYE_INNER))
             self._mp_connections.remove((PoseLandmark.RIGHT_EYE_INNER, PoseLandmark.RIGHT_EYE))
             self._mp_connections.remove((PoseLandmark.RIGHT_EYE, PoseLandmark.RIGHT_EYE_OUTER))
             self._mp_connections.remove((PoseLandmark.RIGHT_EYE_OUTER, PoseLandmark.RIGHT_EAR))
+            self._mp_connections.remove((PoseLandmark.NOSE, PoseLandmark.RIGHT_EYE_INNER))
             self._mp_connections.remove((PoseLandmark.NOSE, PoseLandmark.LEFT_EYE_INNER))
             self._mp_connections.remove((PoseLandmark.LEFT_EYE_INNER, PoseLandmark.LEFT_EYE))
             self._mp_connections.remove((PoseLandmark.LEFT_EYE, PoseLandmark.LEFT_EYE_OUTER))
             self._mp_connections.remove((PoseLandmark.LEFT_EYE_OUTER, PoseLandmark.LEFT_EAR))
-            self._mp_connections.remove((PoseLandmark.MOUTH_RIGHT, PoseLandmark.MOUTH_LEFT))
-            self._mp_connections = self._mp_connections
+            self._mp_connections.remove((PoseLandmark.MOUTH_LEFT, PoseLandmark.MOUTH_RIGHT))
+            self._mp_connections = frozenset(self._mp_connections)
 
         self._mp_pose = self._mp_solution_pose.Pose(
             static_image_mode=static_image_mode,
-            upper_body_only=upper_body_only,
+            model_complexity=1,
             smooth_landmarks=True,
+            enable_segmentation=False,
+            smooth_segmentation=True,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5
         )
@@ -149,8 +170,10 @@ class PoseInterpreter:
             image,
             self.detected_pose.pose_landmarks,
             self._mp_connections,
-            DrawingSpec(color=(255, 0, 0)),
-            DrawingSpec(color=(0, 255, 0))
+            landmark_drawing_spec=self.get_pose_landmarks_style()
+            # DrawingSpec(color=(255, 0, 0)),
+            # DrawingSpec(color=(0, 255, 0))
+            # landmark_drawing_spec=self._mp_drawing_styles.get_default_pose_landmarks_style()
         )
 
     def _calc_angle_if_safe(self, points: tuple):
@@ -168,8 +191,8 @@ class PoseInterpreter:
         landmarks = self.computed_pose["pose_landmarks"]
         for p in points:
             # Check if all 3 landmarks is enough present and visible
-            # if landmarks[p].visibility < VISIBILITY_THRESHOLD or landmarks[p].presence < PRESENCE_THRESHOLD:
-            if landmarks[p].visibility < VISIBILITY_THRESHOLD:
+            # if landmarks[p].visibility < _VISIBILITY_THRESHOLD or landmarks[p].presence < PRESENCE_THRESHOLD:
+            if landmarks[p].visibility < _VISIBILITY_THRESHOLD:
                 return None
 
         # Calc angle
@@ -196,7 +219,7 @@ class PoseInterpreter:
         landmarks = self.computed_pose["pose_landmarks"]
         for p in points:
             # Check if all 3 landmarks is enough present and visible
-            if landmarks[p].visibility < VISIBILITY_THRESHOLD:
+            if landmarks[p].visibility < _VISIBILITY_THRESHOLD:
                 return None
 
         # Calc z angle
@@ -255,10 +278,10 @@ class PoseInterpreter:
                         self.computed_pose["pose_landmarks"][j["pose_landmarks"][1]].math_angle = math_angle
 
         self.computed_ptp = ptp
-        self._post_process_angles()
+        self.post_process_angles()
         return self.computed_pose
 
-    def _post_process_angles(self):
+    def post_process_angles(self):
         """
         Method that check if all joint are in the correct position and not break the servomotors.
         """
@@ -304,35 +327,61 @@ class PoseInterpreter:
         """
 
         image_height, image_width, _ = image.shape
-        font = 0.6
-        margin = 15
+        font = self.ANGLE_TEXT_FONT
+        margin = self.ANGLE_TEXT_MARGIN
 
         for landmark in self.computed_pose["pose_landmarks"]:
             if landmark.angle is not None:  # Draw calculated angles
                 angle_x = landmark.x * image_width
                 angle_y = landmark.y * image_height
                 if margin <= angle_x <= image_width - margin and margin <= angle_y <= image_height - margin:
-                    pos = (int(angle_x) - 15, int(angle_y) + 20)
+                    pos = (int(angle_x) - margin, int(angle_y) + margin + 5)
                     cv2.putText(image, str(int(landmark.angle)), pos,
-                                cv2.FONT_HERSHEY_SIMPLEX, font, (255, 0, 0), 2)
+                                cv2.FONT_HERSHEY_SIMPLEX, font, self.ANGLE_TEXT_COLOR, self.ANGLE_TEXT_THICKNESS)
 
             if self._calc_z and landmark.z_angle is not None:  # Draw calculated z angles if enabled
                 angle_x = landmark.x * image_width
                 angle_y = landmark.y * image_height
                 if margin <= angle_x <= image_width - margin and margin <= angle_y <= image_height - margin:
-                    pos = (int(angle_x) + 20, int(angle_y) + 20)
+                    pos = (int(angle_x) + margin, int(angle_y) + margin)
                     cv2.putText(image, str(landmark.z_angle), pos,
-                                cv2.FONT_HERSHEY_SIMPLEX, font, (255, 255, 255), 2)
+                                cv2.FONT_HERSHEY_SIMPLEX, font, self.ANGLE_Z_TEXT_COLOR, self.ANGLE_TEXT_THICKNESS)
 
             if landmark.math_angle is not None:  # Draw calculated math_angles
                 angle_x = landmark.x * image_width
                 angle_y = landmark.y * image_height
                 if margin <= angle_x <= image_width - margin and margin <= angle_y <= image_height - margin:
-                    pos = (int(angle_x) - 5, int(angle_y) + 20)
+                    pos = (int(angle_x) - 5, int(angle_y) + margin + 5)
                     cv2.putText(image, str(int(landmark.math_angle)), pos,
-                                cv2.FONT_HERSHEY_SIMPLEX, font, (0, 0, 255), 2)
+                                cv2.FONT_HERSHEY_SIMPLEX, font, self.ANGLE_MATH_TEXT_COLOR, self.ANGLE_TEXT_THICKNESS)
 
-    def draw_3d_environment(self):
+    def get_pose_landmarks_style(self):
+        """Returns a pose landmarks drawing style.
+
+        Returns:
+            A mapping from each pose landmark to its default drawing spec.
+        """
+        pose_landmark_style = {}
+        left_spec = DrawingSpec(color=self.POSE_LANDMARKS_LEFT_COLOR, thickness=self.POSE_LANDMARKS_THICKNESS,
+                                circle_radius=self.POSE_LANDMARKS_CIRCLE_RADIUS)
+        right_spec = DrawingSpec(color=self.POSE_LANDMARKS_RIGHT_COLOR, thickness=self.POSE_LANDMARKS_THICKNESS,
+                                 circle_radius=self.POSE_LANDMARKS_CIRCLE_RADIUS)
+        for landmark in _POSE_LANDMARKS_LEFT:
+            pose_landmark_style[landmark] = left_spec
+        for landmark in _POSE_LANDMARKS_RIGHT:
+            pose_landmark_style[landmark] = right_spec
+
+        if not self._display_face_connections:
+            empty_spec = DrawingSpec(color=(0, 0, 0), thickness=0, circle_radius=0)
+            for landmark in self._FACE_LANDMARKS:
+                pose_landmark_style[landmark] = empty_spec
+
+        pose_landmark_style[PoseLandmark.NOSE] = \
+            DrawingSpec(color=self.POSE_LANDMARKS_NOSE_COLOR, thickness=self.POSE_LANDMARKS_THICKNESS,
+                        circle_radius=self.POSE_LANDMARKS_CIRCLE_RADIUS)
+        return pose_landmark_style
+
+    def get_graph_3d_environment(self, animated_azimuth: bool = True):
         """
         Draws landmarks and connections with matplotlib in a new image.
 
@@ -343,15 +392,17 @@ class PoseInterpreter:
 
         if self._plt_fig is None:
             # Initialize at first execution
-            import matplotlib.pyplot as plt
             self._plt_fig = plt.figure()
 
         self._plt_fig.clf()  # Clear figure and configure plot
         ax = plt.axes(projection="3d")
-        ax.set_xlim([2, -1])
-        ax.set_ylim([2.5, -0.5])
-        ax.set_zlim([-3, 3])
-        ax.view_init(270, 90)
+        ax.set_xlim([-1, 2])
+        ax.set_ylim([3, -2])
+        ax.set_zlim([3, -2])
+        ax.view_init(elev=5, azim=self._plt_azim, vertical_axis="y")
+        if animated_azimuth:
+            self._plt_azim += 3
+            self._plt_azim = self._plt_azim % 360
         ax.axes.get_xaxis().set_ticklabels([])
         ax.axes.get_yaxis().set_ticklabels([])
         ax.axes.get_zaxis().set_ticklabels([])
@@ -360,10 +411,13 @@ class PoseInterpreter:
 
             # Zoom in/out axis
             top = self.computed_pose["pose_landmarks"][PoseLandmark.RIGHT_EYE]
-            bottom = self.computed_pose["pose_landmarks"][PoseLandmark.LEFT_HIP]
-            if not self._upper_body_only:
-                bottom = self.computed_pose["pose_landmarks"][PoseLandmark.LEFT_ANKLE]
-            ax.set_ylim([bottom.y + 0.5, top.y - 0.5])
+            bottom = self.computed_pose["pose_landmarks"][PoseLandmark.LEFT_HEEL]
+            if self.computed_pose["pose_landmarks"][PoseLandmark.RIGHT_HEEL].y > bottom.y:
+                bottom = self.computed_pose["pose_landmarks"][PoseLandmark.RIGHT_HEEL]
+            right = self.computed_pose["pose_landmarks"][PoseLandmark.RIGHT_PINKY]
+            left = self.computed_pose["pose_landmarks"][PoseLandmark.LEFT_PINKY]
+            ax.set_ylim([bottom.y, top.y - 0.5])
+            ax.set_xlim([left.x + 0.5, right.x - 0.5])
 
             landmarks = self.computed_pose["pose_landmarks"]
             num_landmarks = len(landmarks)
@@ -374,17 +428,17 @@ class PoseInterpreter:
 
                 if not (0 <= start < num_landmarks and 0 <= end < num_landmarks):
                     continue
-
-                ax.plot(
-                    [landmarks[start].x, landmarks[end].x],
-                    [landmarks[start].y, landmarks[end].y],
-                    [landmarks[start].z, landmarks[end].z]
-                )
+                ax.plot3D(
+                    xs=[landmarks[start].x, landmarks[end].x],
+                    ys=[landmarks[start].y, landmarks[end].y],
+                    zs=[landmarks[start].z, landmarks[end].z],
+                    color=_normalize_color(self.PLOT_COLOR),
+                    linewidth=self.PLOT_THICKNESS)
 
             # Draw calculated angles
             for landmark in landmarks:
                 if landmark.angle is not None:
-                    ax.text(landmark.x, landmark.y, landmark.z, str(landmark.angle), fontsize=8)
+                    ax.text(landmark.x, landmark.y, landmark.z, str(int(landmark.angle)), fontsize=9)
 
         # Draw the render
         self._plt_fig.canvas.draw()
@@ -392,5 +446,5 @@ class PoseInterpreter:
         # Plot canvas to a three channel RGB image represented as numpy ndarray
         image = np.fromstring(self._plt_fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
         image = image.reshape(self._plt_fig.canvas.get_width_height()[::-1] + (3,))
-        image = image[70:400, 150:500]
+        image = image[70:400, 150:520]
         return image
--- main.py
+++ main.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
 
     poseInterpreter = PoseInterpreter(
         config_path="configurations/simple_humandroid.json",
-        upper_body_only=False,
-        face_connections=True,
+        display_face_connections=True,
         calc_z=False
     )
 
@@ -26,7 +25,7 @@ if __name__ == "__main__":
 
         image = cv2.flip(image, 1)  # Flip image for selfie-view
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # BGR image to RGB
-        image.flags.writeable = False  # To improve performance
+        image.flags.writeable = False  # To improve performance, pass by reference
 
         # PoseInterpreter
         poseInterpreter.process_pose_landmark(image)
@@ -47,15 +46,15 @@ if __name__ == "__main__":
         # Build output
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # RGB image to BGR
-        cv2.putText(image, fps, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        cv2.putText(image, fps, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 4)
         cv2.namedWindow("Humandroid body pose V1.0", cv2.WINDOW_NORMAL)
         cv2.imshow("Humandroid body pose V1.0", image)
 
         # Show 3d environment - Comment this to go faster
-        # three_d_env = poseInterpreter.draw_3d_environment()
-        # three_d_env = cv2.cvtColor(three_d_env, cv2.COLOR_RGB2BGR)  # RGB image to BGR
-        # three_d_env = cv2.resize(three_d_env, (0, 0), fx=1.5, fy=1.5)  # Resize image
-        # cv2.imshow('3D environment', three_d_env)
+        three_d_env = poseInterpreter.get_graph_3d_environment()
+        three_d_env = cv2.cvtColor(three_d_env, cv2.COLOR_RGB2BGR)  # RGB image to BGR
+        three_d_env = cv2.resize(three_d_env, (0, 0), fx=1.5, fy=1.5)  # Resize image
+        cv2.imshow('3D environment', three_d_env)
 
         if cv2.waitKey(5) & 0xFF == 27:
             break
